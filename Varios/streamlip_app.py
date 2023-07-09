a9fcdaaa3105fa37ee4286a00b69989558218919
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 18:47:20 -0500

    Update streamlip_app.py

--- Varios/streamlip_app.py
+++ Varios/streamlip_app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Avance 3",
+    page_icon="💻",
+)
+
 def main():
     # Configurar la barra de navegación
     st.sidebar.title("Navegación")
     pages = {
-        "Inicio": show_home,
-        "Página 1": show_page1,
-        "Página 2": show_page2
+        "Inicio": home,
+        "Cargar": pagina1,
+        "Describir": pagina2
+        "Visualizar": pagina3
     }
     page = st.sidebar.selectbox("Ir a", tuple(pages.keys()))
 
@@ -14,9 +20,12 @@ def main():
     pages[page]()
 
 def show_home():
-    st.title("Página de Inicio")
-    st.write("¡Bienvenido a la página de inicio!")
-
+    st.write("# Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
+	st.markdown(
+  	"""Avance 3: Modelos predictivos con aprendizaje automático
+  	### Integrantes:
+  	- Rivera Cumpa Pyerina
+  	""")
 def show_page1():
     st.title("Página 1")
     st.write("Esta es la página 1.")
