c1270387374460e212a1bf5e629e3c20f1f066fb
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 16:34:57 -0500

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
+
+st.expander("## Hidrandina: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
 c1,c2=st.columns([3,7])
 c1.image('descarga.png',width=200)
-c2.markdown("## Hidrandina")
-c2.markdown("### Avance 3, dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
+c2.markdown("### Avance 3: ")
 c2.markdown("#### Integrantes:")
 c2.markdown("###### -Rivera Cumpa Pyerina")
