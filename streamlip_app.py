ccb662498310c8b5ee4dbefce6229d2a48aab0ca
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 16:47:45 -0500

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-
-c1,c2=st.columns([3,7])
-c1.image('descarga.png',width=200)
-c2.markdown("## Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
-c2.markdown("### Integrantes:")
-c2.markdown("###### -Rivera Cumpa Pyerina")
+st.sidebar.write('Siderbar')
+if st.siderbar.button('opcion 1'):
+  c1,c2=st.columns([3,7])
+  c1.image('descarga.png',width=200)
+  c2.markdown("## Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
+  c2.markdown("### Integrantes:")
+  c2.markdown("###### - Rivera Cumpa Pyerina")
