60574739a8ace623d6ffe4d73bfa2e75485f35ac
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 16:44:31 -0500

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-with st.expander("Avance 3"):
-  st.write("Modelos predictivos con aprendizaje automático - Jupyter y Streamlit")
+
 c1,c2=st.columns([3,7])
 c1.image('descarga.png',width=200)
-c2.markdown("## Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
+c2.markdown("## Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
 c2.markdown("### Integrantes:")
 c2.markdown("###### -Rivera Cumpa Pyerina")
