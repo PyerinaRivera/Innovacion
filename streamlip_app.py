74fab38e1c32f88c82099c7d9db1aae6c4db102a
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-08 23:44:20 -0500

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
 
+data=pd.read_csv('DatosAbiertos_consumohdna_202304.csv', encoding='latin-1' , sep=';')
+def main():
+  df=load_data()
+  page=st.sidebar.selectbox("Seleccione el contenido", ["Inicio", "Diccionario", "Manejo de datos"])
