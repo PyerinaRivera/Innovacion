4181d5308aeb6a56578d788c4dcac541c677f931
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 15:55:43 -0500

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 c1,c2=st.columns([3,7])
-c1.image('descarga.png',width=300)
+c1.image('descarga.png',width=200)
 c2.markdown("## Hidrandina")
 c2.markdown("###Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
