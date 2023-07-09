11f210e4c0fd87971b3891bc9e1203de90afd4a6
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 16:38:54 -0500

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-with st.expander("")
+with st.expander(""):
+  st.write("Hola")
 c1,c2=st.columns([3,7])
 c1.image('descarga.png',width=200)
 c2.markdown("## Avance 3: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
