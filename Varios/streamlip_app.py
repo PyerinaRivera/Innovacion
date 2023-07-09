42c80a91412b06164ccba28e0883b3c8ebe652e0
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 18:33:23 -0500

    Update streamlip_app.py

--- Varios/streamlip_app.py
+++ Varios/streamlip_app.py
@@ -10,11 +10,16 @@ with st.siderbar:
 	selected = option_menu(
 		menu_title="Menú", 
 		options=["Inicio","Carga","Describir","Visualizar"],)
-
-
-st.write("# Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
-st.markdown(
-  """Avance 3: Modelos predictivos con aprendizaje automático
-  ### Integrantes:
-  - Rivera Cumpa Pyerina
-  """)
+if selected=="Inicio":
+	st.write("# Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
+	st.markdown(
+  	"""Avance 3: Modelos predictivos con aprendizaje automático
+  	### Integrantes:
+  	- Rivera Cumpa Pyerina
+  	""")
+if selected=="Carga":
+	st.tittle("Aquí")
+if selected=="Describir":
+	st.tittle("Aquí Descr")
+if selected=="Visualizar":
+	st.tittle("Aquí V")
