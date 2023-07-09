47e82d52c59562b4355f859b063182ef40cd2a05
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 18:29:15 -0500

    Update streamlip_app.py

--- Varios/streamlip_app.py
+++ Varios/streamlip_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
 st.set_page_config(
     page_title="Avance 3",
     page_icon="💻",
 )
+
+with st.siderbar:
+	selected = option_menu(
+		menu_title="Menú", 
+		options=["Inicio","Carga","Describir","Visualizar"],)
+
+
 st.write("# Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
 st.markdown(
   """Avance 3: Modelos predictivos con aprendizaje automático
