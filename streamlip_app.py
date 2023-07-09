b83cc8c291a0f8c179400bb0a257dbc3b3f01947
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-08 23:50:29 -0500

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import plotly.express as px
 import seaborn as sns
 
+image = Image.open('descarga.png')
+st.image(image, caption='',use_column_width=True)
+
+st.title("Test de riesgo Covid-19 :sunglasses:")
 data=pd.read_csv('DatosAbiertos_consumohdna_202304.csv', encoding='latin-1' , sep=';')
-def main():
-  df=load_data()
-  page=st.sidebar.selectbox("Seleccione el contenido", ["Inicio", "Diccionario", "Manejo de datos"])
