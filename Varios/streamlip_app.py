fa1a0ff0025522e76516c59a837de97e7ac7aa9d
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 18:52:28 -0500

    Update streamlip_app.py

--- Varios/streamlip_app.py
+++ Varios/streamlip_app.py
@@ -10,9 +10,9 @@ def main():
     st.sidebar.title("Navegación")
     pages = {
         "Inicio": show_home,
-        "Cargar": show_pagina1,
-        "Describir": show_pagina2,
-        "Visualizar": show_pagina3
+        "Página 1": show_page1,
+        "Página 2": show_page2,
+        "Página 3": show_page3
     }
     page = st.sidebar.selectbox("Ir a", tuple(pages.keys()))
 
@@ -20,23 +20,21 @@ def main():
     pages[page]()
 
 def show_home():
-    st.write("# Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
-	st.markdown(
-  	"""Avance 3: Modelos predictivos con aprendizaje automático
-  	### Integrantes:
-  	- Rivera Cumpa Pyerina
-  	""")
-def show_pagina1():
+    st.title("Página de Inicio")
+    st.write("¡Bienvenido a la página de inicio!")
+
+def show_page1():
     st.title("Página 1")
     st.write("Esta es la página 1.")
 
-def show_pagina2():
+def show_page2():
     st.title("Página 2")
     st.write("Esta es la página 2.")
 
-def show_pagina3():
-    st.title("Página 2")
-    st.write("Esta es la página 2.")
+def show_page3():
+    st.title("Página 3")
+    st.write("Esta es la página 3.")
 
 if __name__ == "__main__":
     main()
+
