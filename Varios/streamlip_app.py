3927fc61c90d472dfc4947bcf8e4921997bae0aa
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 18:07:37 -0500

    Add files via upload

---
+++ Varios/streamlip_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Avance 3",
+    page_icon="💻",
+)
+st.write("# Dataset: Consumo energético de clientes Hidrandina [Distriliuz - DLZ]")
+st.markdown(
+  """Avance 3: Modelos predictivos con aprendizaje automático
+  ### Integrantes:
+  - Rivera Cumpa Pyerina
+  """)
+
+st.sidebar.button("Inicio")
