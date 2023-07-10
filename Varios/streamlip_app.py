45876ffb455281426ef23353147089fd2a315a15
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 20:18:33 -0500

    Update streamlip_app.py

--- Varios/streamlip_app.py
+++ Varios/streamlip_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Cargar el dataset y almacenarlo en caché
 @st.cache
 def load_dataset():
-    dataset = pd.read_csv("Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv")
+    dataset = pd.read_csv('Casos_Anemia_Region_Cusco_2010_2020_Cusco.csv', encoding='latin-1' , sep=';')
     return dataset
 
 def main():
