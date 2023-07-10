130b746a205845d1d45cadb3ce9a2532823a619c
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-09 19:06:31 -0500

    Update streamlip_app.py

--- Varios/streamlip_app.py
+++ Varios/streamlip_app.py
@@ -5,9 +5,9 @@ def main():
     st.sidebar.title("Navegación")
     pages = {
         "Inicio": show_home,
-        "Página 1": show_page1,
-        "Página 2": show_page2,
-        "Página 3": show_page3
+        "Cargar": show_page1,
+        "Describir": show_page2,
+        "Visualizar": show_page3
     }
     page = st.sidebar.selectbox("Ir a", tuple(pages.keys()))
 
@@ -16,18 +16,9 @@ def main():
 
 def show_home():
     st.title("Página de Inicio")
-
-    col1, col2 = st.beta_columns([1, 2])
-    with col1:
-        st.image("descarga.png", caption="Descripción de la imagen")
-    with col2:
-        st.header("Hola mundo")
-
-    st.write("Este es un ejemplo de una página de inicio en Streamlit.")
-    st.write("Aquí tienes tres párrafos para mostrar contenido adicional.")
-    st.write("¡Este es el primer párrafo!")
-    st.write("¡Este es el segundo párrafo!")
-    st.write("¡Este es el tercer párrafo!")
+    c1,c2=st.columns([3,7])
+    c1.image('descarga.png', width=200)
+    c2.write("Hola")
 
 def show_page1():
     st.title("Página 1")
