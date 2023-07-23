bffdb6e37951e44eca44ac03f82d17a55370848b
PyerinaRivera <139031691+PyerinaRivera@users.noreply.github.com>
2023-07-23 11:46:00 -0500

    Update streamlip_app.py

--- Varios/streamlip_app.py
+++ Varios/streamlip_app.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 # Cargar el dataset y almacenarlo en caché
 @st.cache
 def load_dataset():
